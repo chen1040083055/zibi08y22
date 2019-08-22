30788d53b58011b085fd1cd552e8d430ae55871c
chen1040083055 <1040083055@qq.com>
2019-08-22 15:07:44 +0800

    提交项目

---
+++ src/main/java/com/newer/zibi08y22/Zibi08y22Application.java
@@ -0,0 +1,13 @@
+package com.newer.zibi08y22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zibi08y22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zibi08y22Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/newer/zibi08y22/Zibi08y22ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.newer.zibi08y22;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Zibi08y22ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
